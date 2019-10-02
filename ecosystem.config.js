733c9d8796b41c6f2890e1225ad1346e19920d77
David Shunfenthal <dshunfenthal@cyberpointllc.com>
2019-10-02 18:41:07 -0400

    Saving round robin issue for later

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       script: 'bin/www',
 
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-      instances: 2,
+      instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '1G'
