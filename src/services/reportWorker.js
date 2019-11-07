7d9142729ba7ef0510a39d1a4673542f6c1895da
David Shunfenthal <dshunfenthal@cyberpointllc.com>
2019-11-06 20:05:40 -0500

    Enabling HTML inlining feature and simplifying generation code

--- src/services/reportWorker.js
+++ src/services/reportWorker.js
@@ -3,6 +3,7 @@ const worker = require('cloth/worker');
 const tmp = require('tmp-promise');
 const path = require('path');
 const fs = require('fs');
+const util = require('util');
 
 const plugins = require('relaxedjs/src/plugins');
 const { preConfigure } = require("relaxedjs/src/config");
@@ -10,8 +11,67 @@ const render = require('relaxedjs/src/render');
 
 const ReportRecord = require('./reportRecord');
 
-
 const puppeteerConfig = preConfigure(false);
+
+const webpack = require('webpack');
+const webpackP = util.promisify(webpack);
+
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+
+const preConfigureWebpack = function(htmlSourcePath, resourceManifestPath, outPath) {
+    return {
+        mode: 'production',
+        entry: resourceManifestPath,
+        output: {
+            path: outPath,
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: [
+                        'style-loader',
+                        'css-loader'
+                    ]
+                },
+                {
+                    test: /\.scss$/,
+                    use: [
+                        'style-loader',
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                },
+                {
+                    test: /\.png$/,
+                    use: [
+                        {
+                            loader: 'url-loader',
+                            options: {
+                                mimetype: 'image/png'
+                            }
+                        }
+                    ]
+                },
+                {
+                    test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+                    use: ['url-loader?limit=10000000']
+                }
+            ]
+        },
+        plugins: [
+            new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
+            new HtmlWebpackPlugin({
+                inject: 'head',
+                template: htmlSourcePath,
+                inlineSource: '.(js)$',
+            }),
+            new HtmlWebpackInlineSourcePlugin()
+        ]
+    };
+};
 
 const workerData = JSON.parse(worker.arguments[0]);
 
@@ -21,6 +81,8 @@ const relaxedGlobals = {
     configPlugins: [],
     basedir: workerData.basedir,
     pageRenderingTimeout: 60,
+    pageWidth: '8.5in',
+    pageHeight: '11in',
 };
 
 
@@ -28,7 +90,7 @@ async function init() {
     await plugins.initializePlugins();
     await plugins.updateRegisteredPlugins(relaxedGlobals, relaxedGlobals.basedir);
 }
-init().then(()=>console.log('initialized ReLaXed'), error => {
+init().then(() => console.log('Initialized ReLaXed'), error => {
     console.error('ReLaXed initialization failed: %o', error);
     process.abort();  // TODO: is this the right way to exit?
 });
@@ -53,26 +115,34 @@ worker.run((message, callback) => {
         }
 
         worker.send('status', ReportRecord.REPORT_STATUSES.GENERATING_HTML);
-        let output = null;
-        if (message.format === 'pdf') {
-            worker.send('status', ReportRecord.REPORT_STATUSES.GENERATING_PDF);
-            const page = await render.browseToPage(puppeteerConfig);
-            try {
-                const tmpdirOptions = {unsafeCleanup: true};
-                output = await tmp.withDir(o => {
-                    let outputPath = devPath || o.path;
-                    const tempHTMLPath = path.resolve(outputPath, 'report.html')
-                    const pdfPath = path.resolve(outputPath, 'report.pdf')
-                    return render.fileToPdf(assetPath, relaxedGlobals, tempHTMLPath, pdfPath, reportData, page);
-                }, tmpdirOptions)
+        const tmpdirOptions = {unsafeCleanup: true};
+        let output = await tmp.withDir(async o => {
+            let outputPath = devPath || o.path;
+            const pugHtmlPath = path.resolve(outputPath, 'report.html')
+            const resourceManifestPath = path.resolve(assetPath, 'index.js');
+            const webpackOutDir = path.resolve(outputPath, 'out');
+            const bundledHtmlPath = path.resolve(outputPath, 'out', 'index.html');
+            const html = await render.generateHtmlFromPath(assetPath, relaxedGlobals, reportData);
+            fs.writeFileSync(pugHtmlPath, html);
+            let stats = await webpackP(preConfigureWebpack(pugHtmlPath, resourceManifestPath, webpackOutDir));
+            if(stats.compilation.errors.length > 0) {
+                const errors = stats.compilation.errors.map(err => err.message).join('\n');
+                console.error(errors);
             }
-            finally {
-                await page.browser().close().catch(console.error);
+            if (message.format === 'pdf') {
+                const pdfPath = path.resolve(outputPath, 'report.pdf')
+                worker.send('status', ReportRecord.REPORT_STATUSES.GENERATING_PDF);
+                const page = await render.browseToPage(puppeteerConfig);
+                try {
+                    return await render.renderPdf(relaxedGlobals, bundledHtmlPath, pdfPath, page)
+                }
+                finally {
+                    await page.browser().close().catch(console.error);
+                }
+            } else {
+                return fs.readFileSync(bundledHtmlPath, 'utf8');
             }
-        }
-        else {
-            // TODO - Render the html with webpack assetization
-        }
+        }, tmpdirOptions);
         return Buffer.from(output, 'binary').toString('base64');  // Base64 encode to safely include in JSON
     })().then(output => callback(null, output), error => callback(error));
 
